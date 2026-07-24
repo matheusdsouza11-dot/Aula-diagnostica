9dbf7b11802560f44c5e4f6da56baa23eeefe306
Matheus Dos Santos Souza - SENAI - Cuiaba/MT <matheus.dsantos@alunosenai.mt>
2026-07-24 10:48:35 -0400

    three commit

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ peso = float(input("Peso (kg): "))
 altura = float(input("Altura (m): "))
 imc = peso / (altura ** 2)
 print(f"Seu IMC é: {imc:.2f}")
+
+import random
+
+numero_secreto = random.randint(1, 10)
+palpite = int(input("Chute um número de 1 a 10: "))
+print(f"Você {'acertou' if palpite == numero_secreto else 'errou'}. O número era {numero_secreto}!")

18071e2371c97b56345e2177147d02bc44d5973a
Matheus Dos Santos Souza - SENAI - Cuiaba/MT <matheus.dsantos@alunosenai.mt>
2026-07-24 10:42:24 -0400

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # Seu primeiro programa em Python
 nome = input("Digite o seu nome: ")
 print(f"Olá, {nome}! Bem-vindo ao mundo do Python.")
+
+
+
+peso = float(input("Peso (kg): "))
+altura = float(input("Altura (m): "))
+imc = peso / (altura ** 2)
+print(f"Seu IMC é: {imc:.2f}")
